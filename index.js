7c1edf1c87b0711bb9fe548bee6993d5f87d50a3
Tom Naorniakowski <tomasz.naorniakowski@itech.media>
2019-01-03 16:46:07 +0100

    refactor main file

--- index.js
+++ index.js
@@ -1,59 +1,77 @@
 'use strict';
 
-var path = require('path'),
-fs = require('graceful-fs'),
-gutil = require('gulp-util'),
-map = require('map-stream'),
-tempWrite = require('temp-write'),
-cachehyperbust = require('cache-hyper-bust');
+var cachehyperbust = require('cache-hyper-bust'),
+	fs = require('graceful-fs'),
+	gutil = require('gulp-util'),
+	map = require('map-stream'),
+	path = require('path'),
+	tempWrite = require('temp-write');
 
-module.exports = function (options) {
-	if(!options){
+module.exports = function(options) {
+	if (!options) {
 		options = {};
 	}
-	return map(function (file, cb) {
+
+	return map(function(file, cb) {
 		if (file.isNull()) {
 			return cb(null, file);
 		}
 
 		if (file.isStream()) {
-			return cb(new gutil.PluginError('gulp-cache-hyper-bust', 'Streaming not supported'));
+			return cb(
+				new gutil.PluginError(
+					'gulp-cache-hyper-bust',
+					'Streaming not supported'
+				)
+			);
 		}
-		if(!options.basePath){
-			options.basePath = path.dirname(path.resolve(file.path))+'/';
+		if (!options.basePath) {
+			options.basePath = path.dirname(path.resolve(file.path)) + '/';
 		}
 
-		tempWrite(file.contents, path.extname(file.path))
-		.then(function (tempFile, err) {
+		tempWrite(file.contents, path.extname(file.path)).then(function(
+			tempFile,
+			err
+		) {
 			if (err) {
 				return cb(new gutil.PluginError('gulp-cache-hyper-bust', err));
 			}
 
-			fs.stat(tempFile, function (err, stats) {
+			fs.stat(tempFile, function(err, stats) {
 				if (err) {
-					return cb(new gutil.PluginError('gulp-cache-hyper-bust', err));
+					return cb(
+						new gutil.PluginError('gulp-cache-hyper-bust', err)
+					);
 				}
-				options = options || {};
 
-				fs.readFile(tempFile, { encoding : 'UTF-8'}, function(err, data) {
+				fs.readFile(tempFile, { encoding: 'UTF-8' }, function(
+					err,
+					data
+				) {
 					if (err) {
-						return cb(new gutil.PluginError('gulp-cache-hyper-bust', err));
+						return cb(
+							new gutil.PluginError('gulp-cache-hyper-bust', err)
+						);
 					}
 
 					// Call the Node module
-					var processedContents = cachehyperbust.busted(data, options);
+					var processedContents = cachehyperbust.busted(
+						data,
+						options
+					);
 
 					if (options.showLog) {
-						gutil.log('gulp-cache-hyper-bust:', gutil.colors.green('✔ ') + file.relative);
+						gutil.log(
+							'gulp-cache-hyper-bust:',
+							gutil.colors.green('✔ ') + file.relative
+						);
 					}
 
 					file.contents = new Buffer(processedContents);
 
 					cb(null, file);
 				});
-
 			});
 		});
 	});
 };
-
